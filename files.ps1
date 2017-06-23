3dfde3a9f970557f07e62b25593b99765131743a
mikeshobes718 <mikeshobes718@gmail.com>
2017-06-23 11:46:32 -0400

    Add files via upload

---
+++ files.ps1
@@ -0,0 +1,8 @@
+﻿#Gets all files like Linux
+clear
+
+#dir c:\Users\u584422
+
+Get-ChildItem C:\Build -recurse | where {! $_.PSIsContainer} | sort Length | Format-Table Name, Length, Directory, Mode, LastWriteTime
+
+Start-Sleep -m 1000000000
