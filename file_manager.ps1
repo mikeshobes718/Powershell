5d698cc8f3ff87b452b29bdf3f2b584ab0393f33
mikeshobes718 <mikeshobes718@gmail.com>
2017-06-28 15:19:06 -0400

    Add files via upload

--- file_manager.ps1
+++ file_manager.ps1
@@ -1,3 +1,21 @@
+﻿clear
+$countDown = 2
+for($i=$countDown;$i-gt0;$i--){
+clear
+if($i-gt1){
+echo "Will began searching in $i seconds"
+}else{
+echo "Will began searching in $i second"
+}
+Start-Sleep -s 1
+}
+
+clear
+echo "Enter CTRL+C to quit program"
+Start-Sleep -s 2
+
+$swatch = [Diagnostics.Stopwatch]::StartNew()
+
 clear
 Clear-Content C:\Users\u584422\Desktop\Script\Powershell\found.txt
 
@@ -7,7 +25,7 @@ $pid_holder = $PID
 #Get all PID's of "file name" and grep it like linux
 $data = Get-WmiObject Win32_Process -Filter "Name='powershell.exe' AND CommandLine LIKE '%file_manager.ps1%'" | Select-String -Pattern "Handle" -CaseSensitive
 
-　
+
 #Get count of times script is running
 $count = (Get-WmiObject Win32_Process -Filter "Name='powershell.exe' AND CommandLine LIKE '%file_manager.ps1%'").count
 
@@ -42,23 +60,34 @@ if($count -gt 1){
 ###################FINDING FILE###################
 #echo "Running $pid_holder..."
 
-$dirType = 1, 2, 3, 4
-$drives  = @()
+clear
 
-echo "Searching..."
+echo "Run the below command in Powershell to tail your output"
+echo "Get-Content C:\Users\u584422\Desktop\Script\Powershell\found.txt -Wait"
 echo ""
+
+echo "Now Searching..."
+echo ""
+
+$dirType = 3
+$drives  = @()
 
 foreach($drive in $dirType) {
     $value = GET-WMIOBJECT win32_logicaldisk -Filter "DriveType=$drive" | ForEach-Object -Process {$_.DeviceID}
     $drives += $value
 }
 
-$timeout = new-timespan -Minutes 90 #Instructions were ten minutes
+$timeout = new-timespan -Minutes 10 #Instructions were ten minutes
 $sw = [diagnostics.stopwatch]::StartNew()
 while ($sw.elapsed -lt $timeout){
-    if (test-path C:){
 
         function Display_File_Details($Fullname,$size,$mode,$psdate,$owner) { 
+            if($sw.elapsed -gt $timeout){
+                echo "`n`nTimed out"
+                break
+                
+            }
+            #start-sleep -seconds 1
             if($mode -NotMatch "d"){
 
             $epoch = [timezone]::CurrentTimeZone.ToLocalTime([datetime]'1/1/1970') 
@@ -68,22 +97,51 @@ while ($sw.elapsed -lt $timeout){
             $file_data | Out-File C:\Users\u584422\Desktop\Script\Powershell\found.txt -append -noClobber
             }
         } 
+    
         foreach($driveArray in $drives){
-        $driven = echo $driveArray
-        echo "Currently Searching the $driven Drive"
-        Get-ChildItem $driven\ -File -Force -Recurse -ErrorAction SilentlyContinue | ForEach-Object { Display_File_Details $_.FullName $_.Length $_.Mode $_.LastWriteTime } #****Works without the Get-ACL command****
-        #Get-ChildItem $driven\ -File -Force -Recurse -ErrorAction SilentlyContinue | ForEach-Object { Display_File_Details $_.FullName $_.Length $_.Mode $_.LastWriteTime ((Get-ACL $_.FullName).Owner) }
+            #$driven = echo $driveArray
+            echo "Currently Searching the $driveArray Drive `n"
+            Get-ChildItem $driven\ -File -Force -Recurse -ErrorAction SilentlyContinue | ForEach-Object { Display_File_Details $_.FullName $_.Length $_.Mode $_.LastWriteTime } #****Works without the Get-ACL command****
+            #Get-ChildItem $driven\ -File -Force -Recurse -ErrorAction SilentlyContinue | ForEach-Object { Display_File_Details $_.FullName $_.Length $_.Mode $_.LastWriteTime ((Get-ACL $_.FullName).Owner) }
         }
 
-        echo ""
-        echo ""
-        echo "Finished!"
-        Start-Sleep -m 1000000000
-        return
-        }
-    else {start-sleep -seconds 5}
+        #echo ""
+        #echo "Finished!"
+        #Start-Sleep -m 1000000000 #Pauses when done searching
+        #return
+
+    #start-sleep -seconds 2
+
+    break
 }
 
-write-host "Timed out"
+$swatch.Stop()
+$time = $swatch.Elapsed
+#$time
 
-###################End of Script################### 
+$hours = $time | ForEach-Object -Process {$_.Hours}
+#$hours
+$minutes = $time | ForEach-Object -Process {$_.Minutes}
+#$minutes
+$seconds = $time | ForEach-Object -Process {$_.Seconds}
+#$seconds
+$milliseconds = $time | ForEach-Object -Process {$_.TotalMilliseconds}
+#$milliseconds
+
+if($hours -gt 0){
+echo ""
+echo "Your Program ran for $hours hours"
+}elseif($minutes -gt 0){
+echo ""
+echo "Your Program ran for $minutes minutes"
+}elseif($seconds -gt 0){
+echo ""
+echo "Your Program ran for $seconds seconds"
+}else{
+echo ""
+echo "Your Program ran for $milliseconds milliseconds"
+}
+
+Start-Sleep -s 60
+
+###################End of Script###################
